82edcbb8242e09c76eb527ef9adb00b44e72d4c4
Steve Willsmore <scwillsmore@gmail.com>
2022-10-31 19:53:40 -0400

    Created for \'hello\' exercise

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
-const sayHello  = function (name) {
-  console.log("Hello, " + name);
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
 }
-
-sayHello('Steve');
+const greeting = returnSayHello('John');
+console.log(greeting);
